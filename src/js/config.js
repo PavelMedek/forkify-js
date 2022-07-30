82169711af77fe0c49c5cacb9b5319a77574d871
PavelMedek <medek41@gmail.com>
2022-07-30 11:36:41 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "7776c952-20d4-4015-8aa8-ffc05c233a3c";
+export const MODAL_CLOSE_SEC = 2.5;
